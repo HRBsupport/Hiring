894fdd504dd4f34410291c76ba9ec87c620eb255
HRBsupport <wakeoneteam@gmail.com>
2026-04-14 02:59:33 +0700

    Create js/sidebar.js

---
+++ js/sidebar.js
@@ -0,0 +1,9 @@
+const sidebarHTML = `
+  <div class="sidebar">
+    <h2>Shinigami</h2>
+    <a href="/index.html">Home</a>
+    <a href="/pages/dashboard.html">Dashboard</a>
+  </div>
+`;
+
+document.getElementById('sidebar').innerHTML = sidebarHTML;
